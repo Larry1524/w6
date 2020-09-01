c4274dccb1d7b8fa17c655c43aef4b542666a26a
Larry1524 <68308643+Larry1524@users.noreply.github.com>
2020-09-01 18:40:01 -0500

    Add files via upload

---
+++ w6larrypy.py
@@ -0,0 +1,60 @@
+#Problem1
+import random
+
+for ran in range(10): 
+ran = random.randrange(25, 35)
+
+print(ran)
+
+#Problem2
+import random
+
+prob = random.random()
+print(prob)
+
+ran = random.randrange(1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99)      
+print(ran)
+
+#Problem3
+
+import random
+
+prob = random.random()
+print(prob)
+
+ran = random.choice(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)      
+print(ran)
+
+#Problem4
+
+import math 
+
+Print(22 / 7)
+
+math.pi
+
+#Problem5
+
+import math
+
+print("How many radians?")
+
+x = input("Enter the radians: ")
+
+math.degrees(x)
+
+#problem6
+
+import math
+
+For f in range(1, 10)
+
+print("please enter your factorial?")
+
+f = input("Enter a number: ")
+
+print(f * (f-1) * (f-2) * (f-3) * (f-4) * (f-5)…)
+
+math.factorial(x)
+
+
